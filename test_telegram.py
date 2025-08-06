5765734f6a1dfd4ebdb81e8c38f45de941611a82
Lucas Ribeiro <57766036+Lucassribeiro9@users.noreply.github.com>
2025-08-06 15:31:36 -0300

    criando o bot

---
+++ test_telegram.py
@@ -0,0 +1,27 @@
+import requests
+import dotenv
+import os
+dotenv.load_dotenv()
+
+token = os.getenv("BOT_TOKEN")
+chat_id = os.getenv("CHAT_ID")
+
+MESSAGE = "Testando o bot"
+
+def send_message(token, chat_id, message):
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    payload = {"chat_id": chat_id, "text": message, "parse_mode": "Markdown"}
+    print("Enviando notificação para o Telegram...")
+    try:
+        response = requests.post(url, data=payload)
+        response.raise_for_status()
+        print("Notificação enviada com sucesso!")
+    except requests.exceptions.RequestException as e:
+        print(f"Erro ao enviar notificação: {e}")
+        print(f"Status code: {e.response.status_code if e.response else 'N/A'}")
+        print(f"Resposta da requisição: {e.response.text if e.response else 'N/A'}")
+
+# Executando a função
+if __name__ == "__main__":
+    send_message(token, chat_id, MESSAGE)
+        
