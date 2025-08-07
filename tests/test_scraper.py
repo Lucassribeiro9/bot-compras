02913f0659befd27bd76828696cdf02d25c2e19b
Lucas Ribeiro <57766036+Lucassribeiro9@users.noreply.github.com>
2025-08-07 10:14:15 -0300

    mudando dados da variavel no teste

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -8,7 +8,7 @@ class TestFormatPrice(unittest.TestCase):
         estimated_price = 3254.07
         self.assertEqual(format_price(brute_price), estimated_price)
     def test_format_price_with_invalid_price(self):
-        brute_price = "R$ 3.254,07"
+        brute_price = "unavailable"
         estimated_price = None
         self.assertEqual(format_price(brute_price), estimated_price)
     def test_format_price_with_empty_price(self):
