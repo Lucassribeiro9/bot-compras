f389fb024cc334d748a2134383e56735bbcf81eb
Lucas Ribeiro <lucas.devlop94@gmail.com>
2025-08-08 14:36:46 -0300

    adicionando comentários

--- notifier_telegram.py
+++ notifier_telegram.py
@@ -1,14 +1,23 @@
-import requests
-import dotenv
+# notificação para o Telegram
+# Este script envia uma mensagem de teste para um bot do Telegram usando a API do Telegram.
 import os
+
+import dotenv
+import requests
+
 dotenv.load_dotenv()
 
+# Variáveis de ambiente
+# Certifique-se de que as variáveis BOT_TOKEN e CHAT_ID estão definidas no arquivo
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
+# Mensagem de teste
 MESSAGE = "Testando o bot"
 
+
 def send_message(message):
+    # Envia uma mensagem de teste para o bot do Telegram
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     payload = {"chat_id": CHAT_ID, "text": message, "parse_mode": "Markdown"}
     print("Enviando notificação para o Telegram...")
@@ -21,7 +30,7 @@ def send_message(message):
         print(f"Status code: {e.response.status_code if e.response else 'N/A'}")
         print(f"Resposta da requisição: {e.response.text if e.response else 'N/A'}")
 
+
 # Executando a função
 if __name__ == "__main__":
     send_message(MESSAGE)
-        
