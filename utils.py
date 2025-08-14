6d3ae60f3ed48a1f8aede859ece9e871ee557fc4
Lucas Ribeiro <57766036+Lucassribeiro9@users.noreply.github.com>
2025-08-14 14:38:08 -0300

    refactor(utils): adicionando tratamento de excecao na funcao format_price

--- utils.py
+++ utils.py
@@ -1,12 +1,18 @@
 # Funções e classes genéricas (Reusabilidade)
 import re
+
+
 # formatando preço
 def format_price(price: str) -> float:
     # Recebe um preço e tenta extrair o valor numérico dele.
     if not price:
         return None
-    clean_price = re.sub(r"[^\d,]", "", price).replace(",", ".")
-    if not clean_price:
-        print("Não foi possível extrair um número válido do preço.")
+    try:
+        clean_price = re.sub(r"[^\d,]", "", price).replace(",", ".")
+        if not clean_price:
+            print("Não foi possível extrair um número válido do preço.")
+            return None
+        return float(clean_price)
+    except (ValueError, AttributeError) as e:
+        print(f"Erro ao formatar o preço: {e}")
         return None
-    return float(clean_price)
