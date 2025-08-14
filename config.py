2b0c772608515c3e5a6d8e4a6a3c9f6f6ca28c0e
Lucas Ribeiro <57766036+Lucassribeiro9@users.noreply.github.com>
2025-08-14 16:10:56 -0300

    feat(config): adicionando site_config

--- config.py
+++ config.py
@@ -7,3 +7,15 @@ DB = "products.db"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 }
+SITE_CONFIG = {
+    "kabum.com.br": {
+        "strategy: static",
+        "name_selector": ("h1", {"class": "text-sm desktop:text-xl text-black-800 font-bold desktop:font-bold"}),
+        "price_selector": ("h4", {"class": "text-4xl text-secondary-500 font-bold transition-all duration-500"}),
+    },
+    "adidas.com.br": {
+        "strategy: dynamic",
+        "name_selector": ("span", {"class": "text-sm desktop:text-xl text-black-800 font-bold desktop:font-bold"}),
+        "price_selector": ("span", {"class": "_sale-color_1dbqy_98"}),
+    },
+}
