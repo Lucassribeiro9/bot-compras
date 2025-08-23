8c31fd6b41f3b4fdeb4406a118b31b066f2b7f4a
Lucas Ribeiro <57766036+Lucassribeiro9@users.noreply.github.com>
2025-08-23 19:38:59 -0300

    feat(bot_app): criando add

--- bot_app.py
+++ bot_app.py
@@ -1,8 +1,8 @@
 # Script que vai fazer o bot rodar 24/7
 import os
 import logging
-import database
-import scrapers.webscraper
+import database as db
+from scrapers import webscraper
 from settings import SITE_CONFIG
 from dotenv import load_dotenv
 from telegram import Update
@@ -26,8 +26,32 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
     welcome_message = (
         f"Olá, {user.first_name}! Bem-vindo ao Bot de Monitoramento de Preços. Use os comandos abaixo:\n"
-        "/add <URL> <Nome do Produto> <Preço Alvo> - Adiciona um produto\n"
+        "/add <URL> <Preço Alvo> - Adiciona um produto\n"
         "/list - Lista todos os produtos\n"
         "/remove <ID do Produto> - Remove um produto da lista.\n"
     )
     await update.message.reply_text(welcome_message)
+
+# add product command
+async def add(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Adiciona produto ao bot"""
+    chat_id = update.effective_chat.id
+    try:
+        url = context.args[0]
+        target_price = float(context.args[1])
+        await update.message.reply_text("Buscando as informações do produto, aguarde...")
+        # Busca o nome
+        info = webscraper.get_product_info(url)
+        if not info or not info.get("name"):
+            await update.message.reply_text("Não foi possível obter as informações do produto. Verifique a URL e tente novamente.")
+            return
+        product_name = info["name"]
+
+        # Adiciona o produto ao banco de dados
+        db.add_product(chat_id, product_name, url, target_price)
+        await update.message.reply_text(f"Produto '{product_name}' adicionado com sucesso! Valor alvo: R$ {target_price:.2f}")
+    except (IndexError, ValueError):
+        await update.message.reply_text("Formato inválido. Use: /add <URL> <Preço Alvo>")
+    except Exception as e:
+        logging.error(f"Erro ao adicionar produto: {e}")
+        await update.message.reply_text("Ocorreu um erro ao adicionar o produto. Tente novamente.")
