010c01c2419d30f9bb4f3cc37ae7fe50e6fd1c04
Lucas Ribeiro <57766036+Lucassribeiro9@users.noreply.github.com>
2025-08-23 18:01:53 -0300

    refactor(main): criando condicao para ler a chave

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
   fingerprint      = var.api_fingerprint
-  private_key_path = var.api_private_key_path
+  private_key      = var.api_private_key_content != null ? var.api_private_key_content : file(var.api_private_key_path)
   region           = var.region
 }
 
