a0480c49f776f4b89f948d2399c2b378d87eb8f1
Lucas Ribeiro <57766036+Lucassribeiro9@users.noreply.github.com>
2025-08-23 00:09:52 -0300

    feat(terraform): script principal, variables e tfplan

---
+++ terraform/main.tf
@@ -0,0 +1,63 @@
+# main.tf
+
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.api_fingerprint
+  private_key_path = var.api_private_key_path
+  region           = var.region
+}
+
+
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = var.tenancy_ocid
+}
+
+data "oci_core_images" "ubuntu_image" {
+  compartment_id           = var.compartment_id
+  operating_system         = "Canonical Ubuntu"
+  operating_system_version = "22.04"
+  shape                    = var.instance_shape
+  sort_by                  = "TIMECREATED"
+  sort_order               = "DESC"
+}
+
+
+resource "oci_core_vcn" "bot_compras_vcn_tf" {
+  compartment_id = var.compartment_id
+  display_name   = "bot-compras-vcn"
+  cidr_block     = "10.0.0.0/16"
+}
+
+resource "oci_core_subnet" "bot_compras_subnet_tf" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.bot_compras_vcn_tf.id
+  display_name   = "bot-compras-subnet"
+  cidr_block     = "10.0.1.0/24"
+}
+
+
+resource "oci_core_instance" "bot_compras_instance_tf" {
+  availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+  compartment_id      = var.compartment_id
+  display_name        = "bot-compras-instance-tf"
+  shape               = var.instance_shape
+  
+  source_details {
+    source_type = "image"
+    source_id   = data.oci_core_images.ubuntu_image.images[0].id
+  }   
+
+  create_vnic_details {
+    subnet_id = oci_core_subnet.bot_compras_subnet_tf.id
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "tenancy_ocid" { type = string }
+variable "compartment_id" { type = string }
+variable "user_ocid" { type = string }
+variable "api_fingerprint" { type = string }
+variable "api_private_key_path" { type = string }
+variable "region" { type = string }
+variable "instance_shape" { type = string }
