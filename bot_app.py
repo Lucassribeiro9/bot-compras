98592d732b7f9335e188fecf21ecb86fdf1281b9
Lucas Ribeiro <57766036+Lucassribeiro9@users.noreply.github.com>
2025-09-09 16:36:29 -0300

    fix(list_products): corrigindo o erro em buscar os produtos ocasionado pelo tipo float no price

--- bot_app.py
+++ bot_app.py
@@ -3,7 +3,7 @@ import os
 import logging
 import database as db
 from scrapers import webscraper
-from utils import format_price
+from utils import format_price_str, format_price
 from settings import SITE_CONFIG
 from dotenv import load_dotenv
 from telegram import Update
@@ -15,6 +15,7 @@ load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 if not TOKEN:
     raise ValueError("TOKEN de acesso ao bot não encontrado nas variáveis de ambiente.")
+
 
 # Start bot
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -29,6 +30,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
     await update.message.reply_text(welcome_message)
 
+
 # add product command
 async def add(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Adiciona produto ao bot"""
@@ -39,25 +41,39 @@ async def add(update: Update, context: ContextTypes.DEFAULT_TYPE):
         target_price_str = context.args[1]
         target_price = format_price(target_price_str)
         logger.debug(f"Buscando informações do produto na URL: {url}")
-        await update.message.reply_text("Buscando as informações do produto, aguarde...")
+        await update.message.reply_text(
+            "Buscando as informações do produto, aguarde..."
+        )
         # Busca o nome
         info = webscraper.get_product_info(url)
         if not info or not info.get("name"):
             logging.error(f"Falha ao obter informações do produto na URL: {url}")
-            await update.message.reply_text("Não foi possível obter as informações do produto. Verifique a URL e tente novamente.")
+            await update.message.reply_text(
+                "Não foi possível obter as informações do produto. Verifique a URL e tente novamente."
+            )
             return
         product_name = info["name"]
 
         # Adiciona o produto ao banco de dados
         db.add_product(url, product_name, target_price, chat_id)
-        logger.info(f"Produto '{product_name}' adicionado com sucesso para o usuário {chat_id}.")
-        await update.message.reply_text(f"Produto '{product_name}' adicionado com sucesso! Valor alvo: R$ {target_price:.2f}")
+        logger.info(
+            f"Produto '{product_name}' adicionado com sucesso para o usuário {chat_id}."
+        )
+        await update.message.reply_text(
+            f"Produto '{product_name}' adicionado com sucesso! Valor alvo: R$ {target_price:.2f}"
+        )
     except (IndexError, ValueError):
         logger.error("Erro de formatação no comando /add.")
-        await update.message.reply_text("Formato inválido. Use: /add <URL> <Preço Alvo>")
+        await update.message.reply_text(
+            "Formato inválido. Use: /add <URL> <Preço Alvo>"
+        )
     except Exception as e:
         logger.error(f"Erro ao adicionar produto: {e}")
-        await update.message.reply_text("Ocorreu um erro ao adicionar o produto. Tente novamente.")
+        await update.message.reply_text(
+            "Ocorreu um erro ao adicionar o produto. Tente novamente."
+        )
+
+
 async def list_products(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Lista os produtos monitorados"""
     try:
@@ -68,8 +84,10 @@ async def list_products(update: Update, context: ContextTypes.DEFAULT_TYPE):
             return
         message = "Produtos monitorados:\n"
         for product in products:
-            target_price_str = format_price(product['target_price'])
-            last_price_str = format_price(product['last_price']) if product['last_price'] is not None else "N/A"
+            target_price_str = format_price_str(product["target_price"])
+            last_price_str = format_price_str(product["last_price"])
+            if product["last_price"] == "Não definido":
+                last_price_str = "Ainda não verificado"
             message += (
                 f"ID: {product['id']}\n"
                 f"Nome: {product['name']}\n"
@@ -77,11 +95,18 @@ async def list_products(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 f"Último Preço: {last_price_str}\n"
                 f"Link: {product['url']}\n\n"
             )
-        logger.info(f"Listando {len(products)} produtos para o usuário {update.effective_chat.id}.")
-        await update.message.reply_text(message, disable_web_page_preview=True, parse_mode='Markdown')
+        logger.info(
+            f"Listando {len(products)} produtos para o usuário {update.effective_chat.id}."
+        )
+        await update.message.reply_text(
+            message, disable_web_page_preview=True, parse_mode="Markdown"
+        )
     except Exception as e:
         logger.error(f"Erro ao listar produtos: {e}")
-        await update.message.reply_text("Ocorreu um erro ao listar os produtos. Tente novamente.")
+        await update.message.reply_text(
+            "Ocorreu um erro ao listar os produtos. Tente novamente."
+        )
+
 
 async def remove(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Remove um produto da lista"""
@@ -90,23 +115,34 @@ async def remove(update: Update, context: ContextTypes.DEFAULT_TYPE):
         success = db.remove_product(product_id)
         if success:
             logger.info(f"Produto com ID {product_id} removido com sucesso.")
-            await update.message.reply_text(f"Produto com ID {product_id} removido com sucesso.")
+            await update.message.reply_text(
+                f"Produto com ID {product_id} removido com sucesso."
+            )
         else:
             logger.warning(f"Produto com ID {product_id} não encontrado.")
-            await update.message.reply_text(f"Produto com ID {product_id} não encontrado. Tente novamente!")
+            await update.message.reply_text(
+                f"Produto com ID {product_id} não encontrado. Tente novamente!"
+            )
     except (IndexError, ValueError):
         logger.error("Erro de formatação no comando /remove.")
-        await update.message.reply_text("Formato inválido. Use: /remove <ID do Produto>")
+        await update.message.reply_text(
+            "Formato inválido. Use: /remove <ID do Produto>"
+        )
     except Exception as e:
         logger.error(f"Erro ao remover produto: {e}")
-        await update.message.reply_text("Ocorreu um erro ao remover o produto. Tente novamente.")
+        await update.message.reply_text(
+            "Ocorreu um erro ao remover o produto. Tente novamente."
+        )
+
 
 def main():
     """Inicia o bot"""
     db.setup_db()
     application = Application.builder().token(TOKEN).build()
     if not application:
-        raise ValueError("Erro ao iniciar o bot. Verifique o TOKEN e a conexão com a internet.")
+        raise ValueError(
+            "Erro ao iniciar o bot. Verifique o TOKEN e a conexão com a internet."
+        )
     # Comandos do bot
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("add", add))
@@ -115,5 +151,7 @@ def main():
 
     logger.info("Bot iniciado com sucesso.")
     application.run_polling()
+
+
 if __name__ == "__main__":
     main()
